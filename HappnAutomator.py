acacd8e2df23a3a7866125c91f0b2f297e13046e
cooldev900 <cooldev900@gmail.com>
2023-11-03 09:17:40 -0500

    feat: create HappnAutomator class

---
+++ HappnAutomator.py
@@ -0,0 +1,12 @@
+class HappnAutomator:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, {}!".format(self.name))
+
+    def get_virtual_number(self):
+        print("virtual number")
+
+    def create_account(self):
+        print("create an account")
